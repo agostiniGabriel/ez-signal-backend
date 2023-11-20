fdaaea528ff0621845ae7a61d867f687109f8f0b
Gabriel Agostini <agostini.gabriel@MacBook-Air-de-Gabriel.local>
2023-11-20 18:40:36 -0300

    adding server status endpoint

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ from flask_restful import Api
 from resources.file_helper import File_Helper
 from resources.audio_time_plot import Audio_Time_Plot
 from resources.audio_spectogram_plot import Audio_Spectogram_Plot
+from resources.server_status import Server_Status
 
 from json import dumps
 
@@ -12,6 +13,7 @@ api = Api(app)
 api.add_resource(File_Helper, '/retrievefile/<file_id>') 
 api.add_resource(Audio_Time_Plot, '/plotAudio/timeDomain')
 api.add_resource(Audio_Spectogram_Plot, '/plotAudio/spectogram')
+api.add_resource(Server_Status, '/status')
 
 if __name__ == '__main__':
     app.run()
