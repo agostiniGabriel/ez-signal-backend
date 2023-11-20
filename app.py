9ac47d90b377b6ce9c6e51025478aa53759611e7
Gabriel Agostini <agostini.gabriel@MacBook-Air-de-Gabriel.local>
2023-11-20 20:55:28 -0300

    testing flask rest

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from flask import Flask
-from flask_restful import Api
+from flask_restful import Api, Resource
 from resources.file_helper import File_Helper
 from resources.audio_time_plot import Audio_Time_Plot
 from resources.audio_spectogram_plot import Audio_Spectogram_Plot
@@ -16,6 +16,13 @@ api.add_resource(Server_Status, '/status')
 @app.route("/")
 def hello_world():
     return "<p>Hello, World!</p>"
+
+class Customers(Resource):
+    def get(self):      
+        return "Test", 200
+    
+# Create API routes
+api.add_resource(Customers, '/customers')
 
 # if __name__ == '__main__':
 #     print('Inicializando App')
