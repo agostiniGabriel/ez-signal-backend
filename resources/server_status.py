c293b5f268a77890f66e8a12ea12e14fd9157fa5
Gabriel Agostini <agostini.gabriel@MacBook-Air-de-Gabriel.local>
2023-11-26 17:08:46 -0300

    trying custom status

--- resources/server_status.py
+++ resources/server_status.py
@@ -3,4 +3,4 @@ from utils.response_utils import success_response
 
 class Server_Status(Resource):
     def get(self):
-        return success_response(body="Server Listening")
+        return success_response(body="Server Listening",custom_status=200)
